52702576c1f39921d087befb240c5705ea441f83
ednakiruai <162158368+ednakiruai@users.noreply.github.com>
2024-03-25 10:40:09 +0300

    solution3.js

--- solution3.js
+++ solution3.js
@@ -1,13 +1,22 @@
+// get the input values
 const basicsalary = 50000;
 const benefits = 10000;
 function calculateNetSalary(basicsalary,benefits){
+    // nhif rate 0.1%
     const NHIFrate = 0.1;
+    // tax rate 0.2%
     const Taxrate = 0.2;
+    // nssf rate 0.06%
     const NSSFrate = 0.06;
+    //calculate grossSalary
     const grossSalary =basicsalary+benefits
+    //calculate NHIF
     const NHIF = grossSalary + NHIFrate;
+    //calcuale Tax
     const Tax = grossSalary + Taxrate;
+    //calculate NSSF
     const NSSF = grossSalary + NSSFrate;
+    //calculate NetSalary
     const NetSalary = grossSalary-(NHIF+Tax+NSSF);
     return{
         grossSalary,
@@ -17,6 +26,7 @@ function calculateNetSalary(basicsalary,benefits){
         NetSalary,
     }
 }
+//shows the output of the calculated solution
 const output = calculateNetSalary(basicsalary,benefits)
 console.log("grossSalary:", output.grossSalary);
 console.log("NHIF:", output.NHIF);
