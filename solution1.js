f2c85ec89a654be45f714c6cddad69852ac5b31c
ednakiruai <162158368+ednakiruai@users.noreply.github.com>
2024-03-25 10:26:04 +0300

    solution1.js

--- solution1.js
+++ solution1.js
@@ -1,12 +1,12 @@
 const grades = 80
 if(grades>79 && grades<=100){
-     console.log("A")
+     console.log("A") //print 'A' if marks are >79
 }else if(grades>59 && grades<=79){
-    console.log("B")
+    console.log("B") //print 'B' if marks are >=60 and <=79
 }else if(grades>48 && grades<=59){
-    console.log("C")
+    console.log("C") //print 'C' if marks are >=50 and <=59
 }else if(grades>39 && grades<=49){
-    console.log("D")
+    console.log("D") // print 'D' if marks are >=40 and <=49
 }else if(grades>=0 && grades<40){
-    console.log("E")
+    console.log("E") // print 'E' if marks are below 39
 }
